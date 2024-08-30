88c58b6aae58faba4808c819fb91da3c93628aef
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-08-30 08:43:56 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-// src/App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import CourseList from './components/course/CourseList';
-import CourseDetails from './components/course/CourseDetails';
 import { Toaster } from 'react-hot-toast';
-import Layout from './components/student/Layout';
-import SidebarComponent from './components/student/SidebarComponent';
 import EnrolledCourse from './components/student/EntrolledCourse';
 import Profile from './components/student/Profile';
 import LogoutPage from './components/student/LogoutPage';
@@ -21,11 +17,9 @@ function App() {
           <Routes>
           <Route path="/" element={<Navigate to="/courses" />} />
           <Route path="/courses" element={<CourseList setSelectedCourse={setSelectedCourse}/>} />
-            <Route path="/course-details" element={<CourseDetails course={selectedCourse} />} />
             <Route path="/course-enroll" element={<EnrolledCourse />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/logout" element={<LogoutPage />} />
-            {/* Add more routes as needed */}
           </Routes>
         </div>
       </div>
